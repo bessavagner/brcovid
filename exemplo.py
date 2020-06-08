9fb9601c199709782d19b322f93966a23c096c08
Vagner Bessa <bessavagner@users.noreply.github.com>
2020-06-08 17:03:59 -0300

    correct extract data

--- exemplo.py
+++ exemplo.py
@@ -2,8 +2,9 @@ import brcovid.api as brc
 from brcovid import get_info
 import numpy as np
 
-local = 'MA'
+local = 'São Luís'
 print(brc.obter_dados(local))
+# get_info.city_cases('São Luís')
 
 """lista = np.array(get_info.list_cities())
 
